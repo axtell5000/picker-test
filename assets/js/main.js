ef5e2843982b32b6a63c8b7e75b09589a9a4e2fc
Stephen Axtell <axtell.stephen@gmail.com>
2019-08-20 16:44:35 +0200

    intiate class on load, add normalize css, add constuctor

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,30 @@ const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'Jul
 
 class DatePicker {
 
-  constructor() {};
+  constructor() {
+    this.insertMonths();
+    this.insertYears();
+  };
   insertMonths() {
     const monthsSel = document.querySelector('#month');
     let option = null;
 
     monthArray.map((month) => {
       option = document.createElement('option');
+      option.value = month;
       option.textContent = month;
       monthsSel.appendChild(option);
     });
   }
+
+  insertYears() {
+    const yearSelect = document.querySelector('#year');
+  }
 }
 
-const test = new DatePicker();
-test.insertMonths();
+// iniate class on load
+window.addEventListener("load", () => {
+  const test = new DatePicker();
+});
+
+
